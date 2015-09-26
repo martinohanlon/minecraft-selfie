8f262618f8829b4e3c13ca7eae93fd87db09dc81
martinohanlon <martin@ohanlonweb.com>
2015-09-26 22:12:22 +0100

    version 0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+#Minecraft Selfie Cam
+##Martin O'Hanlon (martin@ohanlonweb.com)
+##http://www.stuffaboutcode.com
+
+##Description
+A program which takes a picture with either a PC webcam or the Raspberry Pi Camera and renders the image in Minecraft.
+
+Image rendering code based on a project by Ferran Fabregas http://projectlog.ferranfabregas.info/rendering-pi-camera-pictures-into-minecraft-pi-world/
+
+##Structure
+* mcpi - minecraft pi python library
+* mcselfiepc.py - PC python program
+* mcselfiepi.py - Pi python program
+
+##Pre-requisites
+PC program requires VideoCapture python library http://videocapture.sourceforge.net/
+Pi program requires the camera module and picamera python library https://picamera.readthedocs.org/en/release-1.10/ 
+
+##Version history
+* 0.1 - Initial stable version
---
+++ mcselfiepc.py
@@ -0,0 +1,90 @@
+# Martin O'Hanlon
+# www.stuffaboutcode.com
+# Minecraft Selfie Camera
+# Minecraft Picture Rendering Script By Ferran Fabregas (ferri.fc@gmail.com)
+
+from VideoCapture import Device
+from PIL import Image
+import math
+from mcpi.minecraft import Minecraft
+from time import sleep, time
+
+def takePicture(filename):
+    cam = Device()
+    cam.saveSnapshot(filename)
+
+def colormap(pixel):
+    white=(221,221,221)
+    orange=(219,125,62)
+    magenta=(179,80,188)
+    lightblue=(107,138,201)
+    yellow=(177,166,39)
+    lime=(65,174,56)
+    pink=(208,132,153)
+    gray=(64,64,64)
+    lightgray=(154,161,161)
+    cyan=(46,110,137)
+    purple=(126,61,181)
+    blue=(46,56,141)
+    brown=(79,50,31)
+    green=(53,70,27)
+    red=(150,52,48)
+    black=(25,22,22)
+
+    colors=(white,orange,magenta,lightblue,yellow,lime,pink,gray,lightgray,cyan,purple,blue,brown,green,red,black)
+
+    thecolor=0
+    finalresult=256*256*256
+    for idx,color in enumerate(colors):
+        result=math.fabs(color[0]-pixel[0])+math.fabs(color[1]-pixel[1])+math.fabs(color[2]-pixel[2])
+        if result<finalresult:
+            finalresult=result
+            thecolor=idx
+    return thecolor
+
+
+def buildMCImage(mc, filename, pos):
+
+    MAXY = 100
+    #MAXY = 150
+    im = Image.open(filename)
+
+    #resize image file
+    if im.size[1] > MAXY:
+        ratio = MAXY / float(im.size[1])
+        sizeY = MAXY
+        sizeX = int(im.size[0] * ratio)
+        size = sizeX, sizeY
+        im.thumbnail(size, Image.ANTIALIAS)
+    
+    pixels=im.load()
+
+    for x in range (-(im.size[0]/2),(im.size[0]/2)):
+        for y in range (0,(im.size[1])):
+            mc.setBlock(pos.x + x, pos.y + sizeY - y, pos.z - 1, 35,
+                        colormap(pixels[x+(im.size[0]/2) , y]))
+
+            #print "{}.{}.{}".format(x, y, 5)
+            sleep(0.005)
+
+
+mc=Minecraft.create()
+mc.postToChat("Welcome to Minecraft Camera")
+
+while True:
+    #has a block been hit?
+    for hit in mc.events.pollBlockHits():
+        filename = str(int(time())) + ".jpg"
+        mc.postToChat("Taking picture in 1 second")
+        sleep(1)
+        takePicture(filename)
+        mc.postToChat("Building image")
+        buildMCImage(mc, filename, hit.pos)
+        #buildMCImage(mc, "minecon-london.jpg", hit.pos)
+        mc.postToChat("Image built")
+
+        #clear any block hit events
+        mc.events.clearAll()
+
+        break
+    sleep(0.1)
